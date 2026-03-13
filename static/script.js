4d1f8425a8a4f6f32beedec48a017318cb899eb5
Ananya-8 <gptananya0@gmail.com>
2026-03-13 17:44:38 +0530

    Delete static directory

--- static/script.js
+++
@@ -1,9 +0,0 @@
-const video = document.getElementById("video")
-
-navigator.mediaDevices.getUserMedia({ video: true })
-.then(function(stream) {
-    video.srcObject = stream
-})
-.catch(function(err) {
-    console.log("Camera error:", err)
-})
